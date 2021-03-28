9b78689016358329e6baf75b2ef80b7e8969e010
Jxq7 <79334626+Jxq7@users.noreply.github.com>
2021-03-28 21:20:07 +0200

    Update variables-example.js

--- variables-example.js
+++ variables-example.js
@@ -1,3 +1,9 @@
 var myName = "Jxq" // It dosen't have to be your name it can be  other stuff and the variable name dosent have to be "myName" this is just an example.
 
 myName = "1234" // Now later you can set it to be something else if you woud like.
+// note it dosent have to be only your name or something it can be something like this,
+let Console = console.log("Sheluv")
+// Now you might be wondering why i used let instead of "var" we will be talking about that all later.
+
+// Tip: there shoud be some spaces between diffrent lines of code
+let ThisIsaDiffrentNameOfAVariable = console.log("123")
