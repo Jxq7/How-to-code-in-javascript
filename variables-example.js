2af9e715ae9c2049049442e6df6ab13a6b2abbce
Jxq7 <79334626+Jxq7@users.noreply.github.com>
2021-03-28 16:17:00 +0200

    Update variables-example.js

--- variables-example.js
+++ variables-example.js
@@ -1,1 +1,3 @@
-var myName =
+var myName = "Jxq" // It dosen't have to be your name it can be  other stuff and the variable name dosent have to be "myName" this is just an example.
+
+myName = "1234" // Now later you can set it to be something else if you woud like.
